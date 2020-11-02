508515498516e0ba98e53808ef07f50d7088fe62
Daryll <dcapistrano.dev@gmail.com>
2020-11-01 17:38:03 -0800

    update local env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,24 @@
-require("dotenv").config();
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
   siteMetadata: {
-    siteName: 'My Shop',
+    siteName: "Fortress Skate Company",
   },
   plugins: [
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-datocms`,
       options: { apiToken: process.env.DATO_API_TOKEN },
     },
     {
-      resolve: 'gatsby-plugin-snipcart',
+      resolve: "gatsby-plugin-snipcart",
       options: {
-        apiKey: 'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1',
-        autopop: true
-      }
+        apiKey: process.env.SNIPCART_API_KEY,
+        autopop: false,
+      },
     },
   ],
-}
+};
