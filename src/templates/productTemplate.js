ada70ea813b61664a0703cf12a5918cd055f047c
Daryll <dcapistrano.dev@gmail.com>
2020-11-14 21:02:42 -0800

    fix: render correct image for each detail page

--- src/templates/productTemplate.js
+++ src/templates/productTemplate.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
 import Layout from "../layouts/index";
-// import Img from "gatsby-image";
+import Img from "gatsby-image";
 // import Image from "../components/Images/productImage";
 
 // try querying for all the product data and then single it down to the correct image by checking against the pageContext id before rendering on page
+
+// * now that ive got the correct data to each page, reading the docs just reminded me that i should not use static query for page layer data.id
+// * maybe swith this component to use a regualr query if the above is not applicable
 
 export default (props) => (
   <StaticQuery
@@ -25,8 +28,8 @@ export default (props) => (
               productmetadata
               image {
                 url
-                sizes(maxWidth: 300, imgixParams: { fm: "jpg" }) {
-                  ...GatsbyDatoCmsSizes
+                fluid(imgixParams: { maxW: 300 }) {
+                  ...GatsbyDatoCmsFluid
                 }
               }
             }
@@ -42,49 +45,35 @@ export default (props) => (
       });
       console.log(singleProduct);
       if (!products) {
-        // return null;
         console.log("no product found");
       }
       return (
         <Layout site={data.site}>
-          <h1>{props.pageContext.name}</h1>
-          <h1>{props.pageContext.price}</h1>
-          <h1>{props.pageContext.quantity}</h1>
-          <h1>{props.pageContext.id}</h1>
-          <h1>{props.pageContext.productmetadata.type}</h1>
+          <div className="Catalogue__item" key={singleProduct[0].id}>
+            <h1>{props.pageContext.name}</h1>
+            <h1>{props.pageContext.price}</h1>
+            <h1>{props.pageContext.quantity}</h1>
+            <h1>{props.pageContext.id}</h1>
+            <h1>{props.pageContext.productmetadata.type}</h1>
 
-          <p>render here</p>
-          <p>{singleProduct.image}</p>
+            <Img fluid={singleProduct[0].node.image.fluid} />
 
-          {/* <div style={{ maxWidth: `300px` }}>
-          <Image alt="Gatsby in Space" name={props.pageContext.name} />
-        </div> */}
+            <p>{singleProduct[0].node.id}</p>
 
-          {/* below does NOT work with board sizes and throws type error,  */}
+            {/* add working cart button */}
 
-          {/* <h1>{props.pageContext.productmetadata.sizes.small}</h1>
-        <h1>{props.pageContext.productmetadata.sizes.medium}</h1>
-        <h1>{props.pageContext.productmetadata.sizes.large}</h1>
-        <h1>{props.pageContext.productmetadata.sizes.xl}</h1> */}
-
-          {/* fix below to correctly use image */}
-
-          {/* <Img
-          fluid={props.pageContext.image.fluid}
-          alt="Product"
-          style={{ height: `300px`, width: `300px` }}
-        /> */}
-
-          {/* add working cart button */}
-
-          {/* <span
-          className="Product__buy snipcart-add-item"
-          data-item-id={props.pageContext.uid}
-          data-item-price={props.pageContext.price}
-          data-item-name={props.pageContext.name}
-        >
-          add to cart
-        </span> */}
+            <span
+              className="Product__buy snipcart-add-item"
+              data-item-id={props.pageContext.id}
+              data-item-price={props.pageContext.price}
+              // clean up below for production, pass down data from pageContext
+              data-item-image={singleProduct[0].node.image.url}
+              data-item-name={props.pageContext.name}
+              data-item-url={`/`}
+            >
+              add to cart
+            </span>
+          </div>
         </Layout>
       );
     }}
