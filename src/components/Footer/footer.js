0117191eaafb080c63b2a08b562f60756f232c6c
Daryll <dcapistrano.dev@gmail.com>
2020-12-01 20:22:59 -0800

    add copyright and minor style changes to footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,26 +4,27 @@ import Instagram from "../../assets/instagram.svg";
 import Mail from "../../assets/mail.svg";
 
 const Footer = () => (
-  <div className="Footer__Wrap">
-    <div className="Footer">
-      <div className="Social__buttons">
-        <a
-          href="https://instagram.com/fortress_collage/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Instagram />
-        </a>
-        <a href="mailto:fortresscollage@gmail.com">
-          <Mail />
-        </a>
-      </div>
+  <div className="Footer">
+    <div className="Social__buttons">
+      <a
+        href="https://instagram.com/fortress_collage/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Instagram />
+      </a>
+      <a href="mailto:fortresscollage@gmail.com">
+        <Mail />
+      </a>
+    </div>
+    <div style={{ textAlign: `center`, padding: `1em` }}>
+      <p>fortress collage 2020. all rights reserved</p>
+    </div>
 
-      {/* This website is just an example project to demonstrate how you can
+    {/* This website is just an example project to demonstrate how you can
           integrate <a href="https://www.gatsbyjs.org/">Gatsby</a>,{" "}
           <a href="https://snipcart.com/">Snipcart</a> and{" "}
           <a href="https://www.datocms.com">DatoCMS</a>. */}
-    </div>
   </div>
 );
 
