f337978e6b2ea5f9b1b5019540165600499aa8ac
Daryll <dcapistrano.dev@gmail.com>
2020-12-04 12:43:15 -0800

    refactor header.js and .scss

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,7 +6,6 @@ import Cart from "../../assets/cart.svg";
 
 const Header = ({ site }) => (
   <header className="Header">
-    {/* <div className="Header__body"> */}
     <div>
       <h1 className="Header__logo" data-text={site.siteMetadata.siteName}>
         <Link to="/">
@@ -14,16 +13,6 @@ const Header = ({ site }) => (
         </Link>
       </h1>
     </div>
-    {/* <div
-      style={{
-        textAlign: `center`,
-        textTransform: `uppercase`,
-        fontWeight: `bold`,
-        marginTop: `1em`,
-      }}
-    >
-      fortress collage
-    </div> */}
     <div>
       <button className="Header__cart snipcart-checkout snipcart-summary">
         <a href="/" className="Header__cart__link">
@@ -34,21 +23,6 @@ const Header = ({ site }) => (
         </a>
       </button>
     </div>
-    {/* </div> */}
-    {/* <div className="Header__summary__line snipcart-summary">
-          Number of items: <span className="snipcart-total-items"></span>
-        </div>
-        <div className="Header__summary__line snipcart-summary">
-          Total price: <span className="snipcart-total-price"></span>
-        </div> */}
-    {/* <div className="Header__summary snipcart-checkout snipcart-summary">
-        <a href="/" className="Header__summary__cart">
-          <span className="Header__summary__logo">
-            <Cart />
-          </span>{" "}
-          <span className="snipcart-total-items"></span>
-        </a>
-      </div> */}
   </header>
 );
 
