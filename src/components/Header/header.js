71baea98e6c8191234a3778b292e24cc59064963
Daryll <dcapistrano.dev@gmail.com>
2020-12-08 11:00:11 -0800

    fix header positioning on mobile by adding div

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,19 +3,22 @@ import Link from "gatsby-link";
 import BurgerMenu from "./Menu/menu";
 import Logo from "../Logo/logo";
 
-import CartIcon from "../../assets/shopping-bag.svg";
+import CartIcon from "../../assets/bag.svg";
 
 const Header = ({ site }) => (
   <header className="Header">
     <BurgerMenu />
-    <button className="Header__cart snipcart-checkout snipcart-summary">
-      <a href="/" className="Header__cart__link">
-        <div className="Header__cart__icon">
-          <CartIcon />
-          <div className="Header__cart__total snipcart-total-items"></div>
-        </div>
-      </a>
-    </button>
+    <div>
+      <button className="Header__cart snipcart-checkout snipcart-summary">
+        <a href="/" className="Header__cart__link">
+          <div className="Header__cart__icon">
+            <CartIcon />
+            <div className="Header__cart__total snipcart-total-items"></div>
+          </div>
+        </a>
+      </button>
+    </div>
+
     <div className="Header__logo" data-text={site.siteMetadata.siteName}>
       <Link to="/">
         <Logo />
