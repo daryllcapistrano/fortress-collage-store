f84533907d2753a843b7dd35879c493d4807ced4
Daryll <dcapistrano.dev@gmail.com>
2020-12-30 17:51:16 -0800

    hotfix for burger bar positioning

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -7,13 +7,13 @@ import CartIcon from "../../assets/bag.svg";
 
 const Header = ({ site }) => (
   <header className="Header">
-    <Menu />
     <button className="Header__cart snipcart-checkout snipcart-summary">
       <a href="/" className="Header__cart__link">
         <div className="Header__cart__total snipcart-total-items"></div>
         <CartIcon />
       </a>
     </button>
+    <Menu />
     <div className="Header__logo" data-text={site.siteMetadata.siteName}>
       <Link to="/">
         <Logo />
