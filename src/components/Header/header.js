5db29409f231b8f03b5bc1661d4123164c710f1b
Daryll <dcapistrano.dev@gmail.com>
2021-01-01 22:02:04 -0800

    change cart icon to text

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,14 +3,13 @@ import Link from "gatsby-link";
 import Menu from "../Menu";
 import Logo from "../Logo/logo";
 
-import CartIcon from "../../assets/bag.svg";
+// import CartIcon from "../../assets/bag.svg";
 
 const Header = ({ site }) => (
   <header className="Header">
     <button className="Header__cart snipcart-checkout snipcart-summary">
       <a href="/" className="Header__cart__link">
-        <div className="Header__cart__total snipcart-total-items"></div>
-        <CartIcon />
+        CART (<div className="Header__cart__total snipcart-total-items"></div>)
       </a>
     </button>
     <Menu />
